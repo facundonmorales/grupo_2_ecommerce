67429c93228ddbdf41fe24fdbfbd324aa32c8a58
matutecba <matutecba@hotmail.com>
2022-08-28 19:52:07 -0300

    Maty28082022

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const usuariosRouter = require('./routes/routes.users')
 const app = express();
 
+
 app.use(express.static(__dirname + '/public'));
+
+app.set("view engine","ejs");
 
 
 app.listen(3000, ()=>{
@@ -13,16 +17,16 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/login.html');
+// });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-app.get('/pass', (req,res)=>{
-    res.sendFile(__dirname + '/views/pass.html');
-});
+// // app.get('/register', (req,res)=>{
+// //     res.sendFile(__dirname + '/views/register.html');
+// // });
+// app.get('/pass', (req,res)=>{
+//     res.sendFile(__dirname + '/views/pass.html');
+// });
 
 app.get('/productosdetalle', (req,res)=>{
     res.sendFile(__dirname + '/views/productosdetalle.html');
--- routes/routes.users.js
+++ routes/routes.users.js
@@ -3,6 +3,8 @@ const usuariosControllers = require('../controller/users.controller')
 
 const router = express.Router
 
-router.get('/', usuariosControllers.login)
+router.get('/login', usuariosControllers.login),
+router.get('/pass', usuariosControllers.pass),
+router.get('/register', usuariosControllers.register),
 
 module.exports= router;

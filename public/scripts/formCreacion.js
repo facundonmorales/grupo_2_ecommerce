3e2d1b936adb8066bdf105a1f1ad3e99ab7a4785
ManuelUgalde7 <manugaldecv@gmail.com>
2022-11-15 22:47:39 -0300

    arreglando front

--- public/scripts/formCreacion.js
+++ public/scripts/formCreacion.js
@@ -13,23 +13,23 @@ window.addEventListener("load", function () {
    
 
         if (campoNombre.value == "") {
-            alert("el campo nombre tiene que estar completo")
+          errores.push("el campo nombre tiene que estar completo")
         } else if (campoNombre.value.length < 2) {
             alert("el nombre debe tener mas de 2 caracteres")
         };
 
         if (campoPrecio.value == "") {
-            alert("el campo precio tiene que estar completo")
+            errores.push("el campo precio tiene que estar completo")
         };
 
         if (campoCaracteristicas.value == "") {
-            alert("el campo de caracteristicas tiene que estar completo")
+            errores.push("el campo de caracteristicas tiene que estar completo")
         } else if (campoCaracteristicas.value.length < 10) {
-            alert("el nombre debe tener mas de 10 caracteres")
+            errores.push("el nombre debe tener mas de 10 caracteres")
         };
 
          if(campoFotoProducto.value==""){
-            alert("Debe colocar una foto de producto")
+            errores.push("Debe colocar una foto de producto")
          }
 
 
@@ -37,6 +37,8 @@ window.addEventListener("load", function () {
         let ulerrores = document.querySelector("#errores")
         console.log(errores)
         console.log(ulerrores)
+
+        ulerrores.innerHTML=""
 
         if (errores.length > 0) {
             e.preventDefault();
